a75b67e245793553f071b3094e6c040dd4d539e0
jharna234 <jharnar7@gmail.com>
2021-08-16 07:50:17 -0700

    added data interface

--- app/src/main/java/com/acegroup/livelocationtracker/di/ApplicationContextModule.java
+++ app/src/main/java/com/acegroup/livelocationtracker/di/ApplicationContextModule.java
@@ -1,26 +1,26 @@
-//package com.acegroup.livelocationtracker.di;
-//
-//
-//import android.app.Application;
-//import android.content.Context;
-//
-//import javax.inject.Singleton;
-//
-//import dagger.Module;
-//import dagger.Provides;
-//
-//@Singleton
-//@Module
-//public class ApplicationContextModule {
-//
-//    private Application application;
-//
-//    public ApplicationContextModule(Application application){
-//        this.application=application;
-//    }
-//
-//    @Provides
-//    Context getApplicationContext(){
-//        return application;
-//    }
-//}
+package com.acegroup.livelocationtracker.di;
+
+
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+@Singleton
+@Module
+public class ApplicationContextModule {
+
+    private Application application;
+
+    public ApplicationContextModule(Application application){
+        this.application=application;
+    }
+
+    @Provides
+    Context getApplicationContext(){
+        return application;
+    }
+}
--- app/src/main/java/com/acegroup/livelocationtracker/di/Component.java
+++ app/src/main/java/com/acegroup/livelocationtracker/di/Component.java
@@ -1,27 +1,27 @@
-//package com.acegroup.livelocationtracker.di;
-//
-//import com.acegroup.livelocationtracker.utils.authentication.LoginHandler;
-//import com.acegroup.livelocationtracker.utils.authentication.SignupHandler;
-//import com.acegroup.livelocationtracker.view.faculties.FacultyLogin;
-//import com.acegroup.livelocationtracker.view.students.StudentMainActivity;
-//import com.acegroup.livelocationtracker.viewmodel.repository.FacultyRepository;
-//import com.acegroup.livelocationtracker.viewmodel.repository.StudentRepository;
-//
-//import javax.inject.Singleton;
-//
-//@Singleton
-//@dagger.Component(modules = {ApplicationContextModule.class, FirebaseAuthModule.class})
-//public interface Component {
-//
-//    void inject(StudentMainActivity studentMainActivity);
-//
-//    void inject(LoginHandler loginHandler);
-//
-//    void inject(StudentRepository studentRepository);
-//
-//    void inject(FacultyRepository facultyRepository);
-//
-//    void inject(FacultyLogin facultyLogin);
-//
-//    void inject(SignupHandler signupHandler);
-//}
+package com.acegroup.livelocationtracker.di;
+
+import com.acegroup.livelocationtracker.utils.authentication.LoginHandler;
+import com.acegroup.livelocationtracker.utils.authentication.SignupHandler;
+import com.acegroup.livelocationtracker.view.faculties.FacultyLogin;
+import com.acegroup.livelocationtracker.view.students.StudentMainActivity;
+import com.acegroup.livelocationtracker.viewmodel.repository.FacultyRepository;
+import com.acegroup.livelocationtracker.viewmodel.repository.StudentRepository;
+
+import javax.inject.Singleton;
+
+@Singleton
+@dagger.Component(modules = {ApplicationContextModule.class, FirebaseAuthModule.class})
+public interface Component {
+
+    void inject(StudentMainActivity studentMainActivity);
+
+    void inject(LoginHandler loginHandler);
+
+    void inject(StudentRepository studentRepository);
+
+    void inject(FacultyRepository facultyRepository);
+
+    void inject(FacultyLogin facultyLogin);
+
+    void inject(SignupHandler signupHandler);
+}
