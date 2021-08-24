8ff08a4faba7102cd1641754ff5d2db2414a5e28
jharna234 <jharnar7@gmail.com>
2021-08-24 07:57:51 -0700

    various ui level code done

--- app/src/main/java/com/acegroup/livelocationtracker/view/AllConnections.java
+++ app/src/main/java/com/acegroup/livelocationtracker/view/AllConnections.java
@@ -1,16 +1,16 @@
-//package com.acegroup.livelocationtracker.view;
-//
-//import android.os.Bundle;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//import com.acegroup.livelocationtracker.R;
-//
-//public class AllConnections extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_all_connections);
-//    }
-//}
+package com.acegroup.livelocationtracker.view;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.acegroup.livelocationtracker.R;
+
+public class AllConnections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_connections);
+    }
+}
